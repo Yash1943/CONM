fc3b150b4e1adc2e93e5bd3982c0afbfcbe877d1
Yash Pareshbhai Joshi <112645426+Yash1943@users.noreply.github.com>
2023-02-27 07:10:18 +0530

    Create Pr2.c

---
+++ Pr2.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#define f(x) (2*(x * x * x) - (2*x) -5
+void main()
+{
+float x0,x1,x2,f0,f1,f2,e;
+int step=1;
+clrscr();
+up:
+printf("\n Enter the values of x0 and x1\n");
+scanf("%f%f",&x0,&x1);
+printf("Enter tolerate eroor:\n");
+scanf("%f",&e);
+f0 = f(x0);
+f1 = f(x1);
+if( f0 * f1 > 0.0)
+{
+printf("Incorrect inicial gusses. \n");
+goto up;
+}
+printf("\nsteps\t\tx0\t\tx1\t\tx2\t\tf(x2)\n");
+do
+{
+x2 =((x0*f1) - (x1*f0))/(f1-f0);
+f2 = f(x2);
+printf("\n%d\t\t%f\t%f\t%f\t%f\n",step,x0,x1,x2,f2);
+if( f0 * f2 < 0)
+{
+x1 = x2;
+f1 = f2;
+}
+else
+{
+x0 = x2;
+f0 = f2;
+}
+step = step + 1;
+}
+while(fabs(f2)>e);
+printf("\n Root is:%f",x2);
+getch();
+}
